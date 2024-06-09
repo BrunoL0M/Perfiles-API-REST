f821184a55ef6ddc1b6fd9a3becfc0d409a9d7ae
Bruno_OM <89675362+BrunoL0M@users.noreply.github.com>
2024-06-09 12:03:56 -0400

    actualizacion de comentario en la funcion manage.py

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@
 import os
 import sys
 
-
+# FUNCION PARA CORRER TAREAS ADMINS
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'profilesfromdjango.settings')
